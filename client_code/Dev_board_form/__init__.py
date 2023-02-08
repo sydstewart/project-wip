b12784519662c34f7e78f54496e80a58f176a29f
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-02-08 19:05:34 +0000

    Edited Dev_board_form

--- client_code/Dev_board_form/__init__.py
+++ client_code/Dev_board_form/__init__.py
@@ -24,17 +24,20 @@ class Dev_board_form(Dev_board_formTemplate):
     """This method is called when an item is selected"""
 
     records = app_tables.projects_stages.search(tables.order_by('project_column'),project_board= self.board_dropdown.selected_value)
-    dict ={}
+    print('No of Stages',len(records))
+    dicts ={}
     for r in records:
         new_column  = app_tables.stage_translate.get(project_column=r['project_column'])
-#         outofcontrol45above = outofcontrol45above.append({pointdate: df[pointdate].iloc[i-4], pointname: df[pointname].iloc[i-4]}, ignore_index=True)
-        dict([{['project_column'] : "r['project_column']", ['count'] : "r['count']", ['new_column']: new_column['new_column']}])
-#         dict = [{'project_column' : r['project_column'], 'count' : r['count'], 'new_column': new_column['new_column']}]
-    print(dict)
+        dicts = ([{'project_column' : r['project_column'],'project_board': r['project_board'],'count':r['count'], 'new_column': new_column['new_column']}])
+#         print(dicts)
 #     new_columns = app_tables.stage_translate.search(tables.order_by('new_column'))
-    self.plot_1.layout.title= self.board_dropdown.selected_value
-#     if self.board_dropdown.selected_value == 'Development':
-    self.plot_1.data = go.Bar(x=[r['new_column'] for r in dict],#r['project_column'] for r in records],
-                              y=[r['count'] for r in dict])
+        
+    print(dicts)
+#         for r in dicts:   
+        
+#               self.plot_1.layout.title= self.board_dropdown.selected_value
+      
+#               self.plot_1.data = go.Bar(x=[r['new_column'] for r in dicts] ,
+#                                     y=[r['count'] for r in dicts])
     pass
-
+ 
