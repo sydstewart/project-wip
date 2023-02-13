93304cce1125c6d2108f46bc4b8371516efa402c
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-02-13 11:56:05 +0000

    Edited charts

--- server_code/charts.py
+++ server_code/charts.py
@@ -8,23 +8,10 @@ import plotly.graph_objects as go
 import plotly.express as px
 @anvil.server.callable
 def bar_charts(dicts):
-#   barcharts = pd.DataFrame()
-#   records = app_tables.projects_stages.search(tables.order_by('project_column'),project_board= board)
-   
-#   for r in records:
-#           dicts = [{'project_column' : r['project_column'],'project_board': r['project_board'],'count':r['count'], 'new_column': new_column['new_column']} for r in records]
 
       df = pd.DataFrame.from_dict(dicts)
       df.sort_values(by= 'new_column', inplace=True)
-#   df = df.groupby('project_board').sum()
-#   barchart = go.Bar( 
-#             x=df['new_column'] ,
-#             y=df['count'],
-#             )
-# #             color = df[ 'project_board'])
-# #             mark_color = df['project_board'])             
-# #   x=dfg['name'], y=dfg['%']) for group, dfg in df.groupby(by='week')]          
-#   return barchart
+      print(df)
        
       fig = px.bar( df, x=df['new_column'] , y= df['count'] , color =df['project_board'] ,title="Project Stages")
       return fig 
