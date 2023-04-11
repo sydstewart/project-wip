8e282478f7a2967c0efef641956f24551435582c
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-04-11 16:52:51 +0000

    Created project_list_search; Edited project_list.RowTemplate2 and project_list

---
+++ client_code/project_list_search/__init__.py
@@ -0,0 +1,18 @@
+from ._anvil_designer import project_list_searchTemplate
+from anvil import *
+import anvil.users
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class project_list_search(project_list_searchTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.repeating_panel_1.items = app_tables.projects.search()
+
+  def project_name_searchbox_change(self, **event_args):
+    """This method is called when the text in this text box is edited"""
+    self.repeating_panel_1.items = app_tables.projects.search(project_name = q.like('%'  + self.project_name_searchbox.text + '%'))
+    pass
