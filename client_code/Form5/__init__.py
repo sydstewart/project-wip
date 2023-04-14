bc1cf7028a00567ab31c08c7b62df4b5a4acf8e2
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-04-14 14:40:09 +0000

    Created Form5; Edited Form4 and settings

---
+++ client_code/Form5/__init__.py
@@ -0,0 +1,43 @@
+from ._anvil_designer import Form5Template
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Form5(Form5Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def Logout_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.content_panel.clear()
+    anvil.users.logout()
+    
+    anvil.users.login_with_form()
+    open_form('Form5') 
+    
+    
+    
+    pass
+
+
+
+
+def Logout_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.content_panel.clear()
+    self.column_panel_1.clear()
+    anvil.users.logout()
+    
+    anvil.users.login_with_form()
+    open_form('home') 
+    
+    
+    
+    pass
+
