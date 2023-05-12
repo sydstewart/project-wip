ff37234f64f097c097a0356df848d1b953559295
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-05-12 07:42:40 +0000

    Edited active_projects_new_column_report

--- client_code/active_projects_new_column_report/__init__.py
+++ client_code/active_projects_new_column_report/__init__.py
@@ -10,7 +10,8 @@ class active_projects_new_column_report(active_projects_new_column_reportTemplat
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    boards =list({(r['project_board']) for r in app_tables.projects.search(tables.order_by('project_board'))})
+    self.project_board_dropdown.items = boards
     # Any code you write here will run before the form opens.
     dicts_boards =anvil.server.call('active_board_stages')
     dicts_boards=sorted(dicts_boards, key = lambda i: (i['project_board'], i['new_column']))
@@ -23,5 +24,8 @@ class active_projects_new_column_report(active_projects_new_column_reportTemplat
 
   def project_board_dropdown_change(self, **event_args):
     """This method is called when an item is selected"""
+    dicts_boards =anvil.server.call('active_board_stages')
+    dicts_boards=sorted(dicts_boards, key = lambda i: (i['project_board'], i['new_column']))
+    self.repeating_panel_1.items = dicts_boards
     pass
 
