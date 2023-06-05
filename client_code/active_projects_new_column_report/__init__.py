e4e062fecfdc2b180d3f4746c16c545a95ba825a
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-06-05 12:33:36 +0000

    Edited active_projects_new_column_report

--- client_code/active_projects_new_column_report/__init__.py
+++ client_code/active_projects_new_column_report/__init__.py
@@ -15,29 +15,11 @@ class active_projects_new_column_report(active_projects_new_column_reportTemplat
     # Any code you write here will run before the form opens.
     dicts_boards =anvil.server.call('active_board_stages')
     print(dicts_boards)
+    k = 'project_boards'
     filter_string= 'API Upgrades'
-    dicts ={}
-    result =dicts(
-                (k,v)
-                for k,v in dicts_boards() if filter_string in k)
-    # newl = [d for d in dictn if d['project_board'] = filter_string]
-    # filtered_dict = dict((d['Name'], d) for d in newl)
-    # filter_string = "Maintenance Upgrades, Server Moves, Add on Modules"
-    # result= [dicts_boards[i] for i in filter_string if i in dicts_boards]
-    print(result)
-    k='project_board'
-    result = {k:v for k,v in dicts_boards.items if filter_string in k}
-    # result = {key:value for (key, value) in dicts_boards.items() if key = "Maintenance Upgrades, Server Moves, Add on Modules"}
-    # dicts_boards = {key:value for (key, value) in dicts_boards.items() if value >= 170}
-    self.repeating_panel_1.items = result
-
-  def text_area_1_change(self, **event_args):
-    """This method is called when the text in this text area is edited"""
-
-    pass
-
-  def project_board_dropdown_change(self, **event_args):
-    """This method is called when an item is selected"""
-    search_using_kwargs(self)
-    pass
-
+    res = []
+    d={}
+    key: value for key, value in d1.items() if value > 0}
+    for k,v in dicts_boards.items():
+       if v = filter_string:
+       res[k]=v
