7735a9dc251e348e041c74bb9539954fcbb51ae5
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2024-05-03 09:52:51 +0000

    Edited burndown_records

--- client_code/burndown_records/__init__.py
+++ client_code/burndown_records/__init__.py
@@ -10,11 +10,12 @@ from anvil.tables import app_tables
 class burndown_records(burndown_recordsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
-   self.Order_no_dropdown.items =  
-   self.repeating_panel_2.items = app_tables.burndown.search()
+   orders =list({(r['order_no']) for r in app_tables.burndown.search(tables.order_by('order_no',ascending = True))})
+   self.Order_no_dropdown.items = orders 
+   self.repeating_panel_2.items = app_tables.burndown.search(tables.order_by('timeline_date',ascending = False))
     # Any code you write here will run before the form opens.
 
   def Order_no_dropdown_change(self, **event_args):
     """This method is called when an item is selected"""
-    self.repeating_panel_2.items = app_tables.burndown.search(order_no=self.Order_no_dropdown.selected_value)
+    self.repeating_panel_2.items = app_tables.burndown.search( tables.order_by('timeline_date',ascending = False), order_no=self.Order_no_dropdown.selected_value)
     pass
