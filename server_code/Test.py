8d8cc789d2baae28bd00259253d5a8a00c3d3822
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2024-07-19 12:55:20 +0000

    New Chart

--- server_code/Test.py
+++ server_code/Test.py
@@ -243,15 +243,36 @@ def get_run_chart():
 
 @anvil.server.callable
 def wip_run_chart():
-   # import plotly.graph_objects as go
+   import plotly.graph_objects as go
    import plotly.express as px 
    chart_data = app_tables.daily_wip.search(Date_of_WIP= q.greater_than(date(year=2024, month=7, day=17)))
    dicts = [{'Date_entered': r['Date_of_WIP'], 'Total Order Value': r['Total_Order_Value'], 'Total Work To Do Value': r['Total_Work_To_Do_Value']} for r in chart_data]
    df = pd.DataFrame.from_dict(dicts)
    print('df',df)
-   fig = px.line(df, x= 'Date_entered', y='Total Order Value', markers=True)
-   # line_plots = go.Scatter(x=df['Date_entered'], y=df['Total Order Value'], name='Total Order Value', marker=dict(color='#e50000'))
-   fig.add_scatter(x=df['Date_entered'], y=df['Total Work To Do Value'], mode='markers+lines')
+
+   fig = go.Figure([
+    go.Scatter(
+        name='Total Order Value',
+        x=df['Date_entered'],
+        y=df['Total Order Value'],
+        mode='lines+markers',
+        marker=dict(color='red', size=4),
+        line=dict(width=1),
+        showlegend=True)
+    ,
+    go.Scatter(
+        name='Total Work To Do Value',
+        x=df['Date_entered'],
+        y=df['Total Work To Do Value'],
+        mode='lines+markers',
+        marker=dict(color='blue', size=4),
+        line=dict(width=1),
+        showlegend=True)
+   ])
+   # fig = px.line(df, x= 'Date_entered', y='Total Order Value', markers=True)
+   # fig['data'][0]['name']='Total Order Value'
+   # # line_plots = go.Scatter(x=df['Date_entered'], y=df['Total Order Value'], name='Total Order Value', marker=dict(color='#e50000'))
+   # fig.add_scatter(x=df['Date_entered'], y=df['Total Work To Do Value'], mode='markers+lines', name= 'Total Work To Do Value')
    return fig
 @anvil.server.callable
 def testprojects():
