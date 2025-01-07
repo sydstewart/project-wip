70bb4139c55460af932500f07976d4cfed01c086
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2025-01-07 16:58:59 +0000

    Created list_projects_pdf; Edited list_projects and projects

--- client_code/list_projects/__init__.py
+++ client_code/list_projects/__init__.py
@@ -41,7 +41,7 @@ class list_projects(list_projectsTemplate):
     """This method is called when an item is selected"""
     print('Percent Complete=', self.percent_complete_text_box.text)
     print('Assigned to =', self.drop_down_1.selected_value)
-    print('Catgeory =', self.Category.selected_value)
+    print('Category =', self.Category.selected_value)
     dicts,Xmedia = anvil.server.call('get_orders', self.percent_complete_text_box.text,self.drop_down_1.selected_value, self.Category.selected_value)
     self.text_box_1.text = len(dicts)
     self.repeating_panel_1.items = dicts
@@ -50,11 +50,17 @@ class list_projects(list_projectsTemplate):
   def download_button_click(self, **event_args):
     """This method is called when the button is clicked"""
     # start = self.level_textbox.text
-    dicts, Xmedia = anvil.server.call('get_orders', self.percent_complete_text_box.text,self.drop_down_1.selected_value, self.Category.selected_value)
-    # download(Xmedia)
-    pdfdata =anvil.server.call('create_projects_pdf', dicts)
-    download(pdfdata)
-    # anvil.media.download(media_object)
+    pdf_info = {
+        'percent_complete': self.percent_complete_text_box.text,
+        'assigned to': self.drop_down_1.selected_value,
+         'category' : self.Category.selected_value
+    }
+    anvil.server.call('generate_pdf', pdf_info)
+    # dicts, Xmedia = anvil.server.call('get_orders', self.percent_complete_text_box.text,self.drop_down_1.selected_value, self.Category.selected_value)
+    # # download(Xmedia)
+    # pdfdata =anvil.server.call('create_projects_pdf', dicts)
+    # download(pdfdata)
+    # # anvil.media.download(media_object)
     pass
     pass
 
--- server_code/projects.py
+++ server_code/projects.py
@@ -97,6 +97,60 @@ def get_orders(percent_complete,assigned_to, category):
 
 
 @anvil.server.callable
-def create_projects_pdf(dicts):
-  media_object = anvil.pdf.render_form('list_projects')
-  return media_object
+def generate_pdf(pdf_info):
+  # media_object = anvil.pdf.render_form('list_projects')
+   print('pdf_info =', pdf_info)
+   assigned_to =pdf_info['assigned to']
+   percent_complete = pdf_info['percent_complete']
+   category = pdf_info['category']
+   with conn.cursor() as cur:
+                cur.execute(
+                      "Select sales_orders.name As name, sales_orders.date_entered As date_entered, \
+                        CONCAT(sales_orders.prefix,sales_orders.so_number) As so_number, sales_orders.so_stage As so_stage, \
+                        sales_orders.subtotal_usd AS Order_Value, \
+                      sales_orders_cstm.workinprogresspercentcomplete_c AS workinprogresspercentcomplete_c,\
+                      sales_orders_cstm.OrderCategory AS OrderCategory,\
+                      sales_orders.so_number AS so_number,\
+                      users.user_name AS user_name, \
+                      sales_orders_cstm.workinprogresspercentcomplete_c AS workinprogresspercentcomplete_c \
+                      From sales_orders\
+                      INNER JOIN `sales_orders_cstm` ON (`sales_orders`.`id` = `sales_orders_cstm`.`id_c`)\
+                      LEFT JOIN `users` ON (`sales_orders`.`assigned_user_id` = `users`.`id`) \
+                      Where sales_orders.date_entered > '2020-01-01' AND \
+                          sales_orders_cstm.OrderCategory NOT IN ('Maintenance') AND \
+                          sales_orders.so_stage  NOT IN ('Closed', 'On Hold','Cancelled','Complete')"
+                            )  
+   records = cur.fetchall()
+   number_of_records =len(records)
+   print('No of projects',number_of_records)
+  
+   if number_of_records:
+              dicts = [{'order_no': r['so_number'], 'project_name':r['name'] ,'order_date':r['date_entered'], 'order_category':r['OrderCategory'],'assigned_to':r['user_name'] , \
+                      'order_value':r['Order_Value'], 'percent_complete':r['workinprogresspercentcomplete_c'] } \
+                      for r in records]
+   print(dicts)
+   X = pd.DataFrame.from_dict(dicts)
+    
+   X['percent_complete'] = X['percent_complete'].fillna(0)
+    # print('before',X['percent_complete'])
+    
+   X['percent_complete']= X['percent_complete'].astype(int)
+    
+   if assigned_to and not category:
+        filter = (X['assigned_to'] == assigned_to)
+        X =  X[X['percent_complete'] > int(percent_complete) ]
+        X =  X[filter]
+   elif category and not assigned_to:
+          X =  X[X['percent_complete'] > int(percent_complete)]
+          X =  X[X['order_category'] == category]
+   elif assigned_to and category:
+          X =  X[X['percent_complete'] > int(percent_complete) ]
+          filter = (X['assigned_to'] == assigned_to)
+          X =  X[filter]
+          X =  X[X['order_category'] == category]
+  
+    # print('after filter',X['percent_complete'])
+   X['percent_complete'] = X['percent_complete'].map(str)
+   dicts =X.to_dict(orient='records')
+   media_object = anvil.pdf.render_form('list_projects_pdf',dicts)
+   return media_object 
