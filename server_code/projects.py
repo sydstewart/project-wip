d1d60be53d913bde925af4c477a6c5267ce3df12
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2025-03-18 11:04:14 +0000

    Edited projects

--- server_code/projects.py
+++ server_code/projects.py
@@ -98,6 +98,7 @@ def get_orders(percent_complete,assigned_to, category):
     today = datetime.today()
     X['days_elapsed'] = (today - X['order_date']).dt.days
     print(X['days_elapsed'])
+   # X['Value Left to Do'] = (100 - X['percent_complete']) 
     X['Year'] = X['order_date'].dt.year
     X['Month']= X['order_date'].dt.month
     X['Day']= X['order_date'].dt.day
