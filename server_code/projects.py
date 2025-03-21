1f939fb17d39fcdd187a4f981ce00b5e9da701d6
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2025-03-21 09:35:03 +0000

    Created Form9; Edited Work_in_Progress, list_projects and projects

--- server_code/projects.py
+++ server_code/projects.py
@@ -57,7 +57,7 @@ def get_orders(percent_complete,assigned_to, category,bool_value):
                       LEFT JOIN `users` ON (`sales_orders`.`assigned_user_id` = `users`.`id`) \
                       Where sales_orders.date_entered > '2020-01-01' AND \
                           sales_orders_cstm.OrderCategory NOT IN ('Maintenance') AND \
-                           sales_orders.so_stage  NOT IN ('Closed', 'On Hold','Cancelled','Complete')")
+                         sales_orders.so_stage  NOT IN ('Closed', 'On Hold','Cancelled','Complete')")
       
     records = cur.fetchall()
     number_of_records =len(records)
