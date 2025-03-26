c343b537b1b3ddfef80ac15908cfc39cf10dd637
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2025-03-26 10:34:21 +0000

    Edited tallies and list_projects

--- client_code/tallies.py
+++ client_code/tallies.py
@@ -8,7 +8,26 @@ def tallies(self, dicts):
 
     average_completion = sum([int(x['percent_complete']) / len(dicts)  for x in dicts])
     subtotal = sum([int(x['order_value'] ) for x in dicts])
+    # invoiced_total = sum([int(x['partially_invoiced_total'] ) for x in dicts])
+    work_completed = (subtotal * average_completion)/ 100
     work_to_do = (subtotal * (100 - average_completion))/ 100
-    self.total_value_label.text = f"TOTAL_VALUE_OF_ORDERS : £{subtotal:,}"
-    self.average_completion_label.text = f"Average_COMPLETION : {average_completion:.1f}%"
+    self.total_value_label.text = f"TOTAL_VALUE_OF_ORDERS: £{subtotal:,}"
+    self.average_completion_label.text = f"Average_COMPLETION     : {average_completion:.1f}%"
     self.work_to_do_label.text = f"Value_of_Work_TO_DO : £{work_to_do:,}"
+    # self.invoiced_total.text =  f"TOTAL_INVOICED    : £{invoiced_total:,}"
+    # self.invoiced_total_copy.text =  f"£{invoiced_total:,}"
+    # self.work_completed.text = f"£{work_completed :,}" 
+    # invoiced_but_not_completed = invoiced_total - work_completed 
+    # # self.invoiced_but_not_completed.text = f"£{invoiced_but_not_completed:,}"
+    # self.total_order_value.text =  f"£{subtotal :,}" 
+    # self.work_completed_top.text =  f"WORK COMPLETED : £{work_completed:,}"
+    # if invoiced_but_not_completed >= 0:
+    #      self.invoiced_but_not_completed.text = f"£{invoiced_but_not_completed:,}"
+    #      self.top_invoiced_but_not_completed.text = f"TOTAL_INVOICED_NOT_COMPLETE: £{invoiced_but_not_completed:,}"
+    # else:
+    #      self.invoiced_but_not_completed.text = f"-£{abs(invoiced_but_not_completed):,}"
+    #      self.top_invoiced_but_not_completed.text = f"TOTAL_INVOICED_NOT_COMPLETE: -£{abs(invoiced_but_not_completed):,}"
+
+    # dicttotals = [{'order_no': 'totals', 'project_name':' ' ,'order_date':' ', 'order_category':' ','assigned_to':' ', \
+    #                   'order_value':f"£{subtotal :,}", 'percent_complete':f"{average_completion:.1f}%", 'partially_invoiced_total':invoiced_total} ] 
+    # print('dicttotals', dicttotals)
