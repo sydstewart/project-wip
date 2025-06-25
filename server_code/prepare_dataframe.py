e6be0ec45dbe58503380eb556ed0943a4fe89004
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2025-06-25 08:55:37 +0000

    Edited prepare_dataframe

--- server_code/prepare_dataframe.py
+++ server_code/prepare_dataframe.py
@@ -155,7 +155,7 @@ def prepare_pandas(dicts, percent_complete,hi_percentage, assigned_to, category,
     pd.set_option('display.max_columns', None)
     print('pivotsyd',pivotsyd)
   
-    X = X[(X['Stage Group'] =='Project in Progress')] 
+    # X = X[(X['Stage Group'] =='Project in Progress')] 
     X1 = X[['order_no','project_name', 'percent_complete', 'order_value_formated','partially_invoiced_total_formated','Value yet to be invoiced_formated','days_elapsed','Value yet to be invoiced', 'assigned_to']]
     X1 = X1.sort_values(by='percent_complete',ascending=False)
     X2 = X1.sort_values(by='Value yet to be invoiced_formated',ascending=False)
