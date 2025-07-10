6c3873458eb18454bf734882d7f5ec0a58aea79b
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2025-07-10 12:25:21 +0000

    Created pie_chart_new; Edited Run_Chart_New, pie_charts_new and Form9; Deleted pie_chart_new

--- client_code/Form9/__init__.py
+++ client_code/Form9/__init__.py
@@ -5,27 +5,37 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from .. import pie_charts_new
+from ..pie_charts_new import pie_charts_new
+from ..pivots_new import pivots_new
+from ..Run_Chart_New import Run_Chart_New
 
 class Form9(Form9Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-   
-    # Any code you write here will run before the form opens.
-
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('pie_charts_new')
+    self.link_3_click()
     
-    pass
-
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
     # self.reset_links()
     # self.link_1.role = 'selected'
-
     self.content_panel.clear()
     self.content_panel.add_component(pie_charts_new(), full_width_row=True)
     # self.reset_links()
     pass
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+
+    self.content_panel.clear()
+    self.content_panel.add_component(pivots_new(), full_width_row=True)
+    # self.reset_links()
+    pass
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(Run_Chart_New(), full_width_row=True)
+    # self.reset_links()
+    pass
+ 
