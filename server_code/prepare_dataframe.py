daf16763c376fc5bc09d1f756bf0a74b73aeb563
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2025-07-11 17:28:07 +0000

    Edited prepare_dataframe

--- server_code/prepare_dataframe.py
+++ server_code/prepare_dataframe.py
@@ -74,8 +74,18 @@ def prepare_pandas(dicts, percent_complete,hi_percentage, assigned_to, category,
     # utc_now = datetime.utcnow
   # naive_utc = datetime.utcnow()
     # today = datetime.utcnow() 
-    # X['days_elapsed'] = (today - X['order_date']).dt.days
-    # # print(X['days_elapsed']) =  X['days_elapsed'])
+    # today = datetime.now(anvil.tz.tzutc())
+    today = datetime.today().strftime('%d/%m/%Y')
+    monthtoday = date(today).dt.month
+    print('monthtoday', monthtoday)
+    X['order_date'] = pd.to_datetime(X.order_date).dt.strftime('%d/%m/%Y')
+    print('Today',today)
+    print('Order Date',X['order_date'])
+    X['days_elapsed'] = (today - X['order_date']).dt.days
+    d0 = date(2008, monthtoday, 18)
+    d1 = date(2008, 9, 26)
+    delta = d1 - d0
+    # print(X['days_elapsed']) =  X['days_elapsed'])
     X['Work Completed'] =X['order_value'] * X['percent_complete']/100
     X['Work Completed_formated'] = X['Work Completed'].map("£{:,.0f}".format)
     X['Work To Do'] =X['order_value'] * (100 - X['percent_complete'])/100
