258adb68e3d804f23c7e78501a6718c1fd3559a2
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2025-08-02 11:14:30 +0000

    Edited Testquery and orders

--- client_code/Testquery/__init__.py
+++ client_code/Testquery/__init__.py
@@ -5,13 +5,48 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+from datetime import datetime, time , date , timedelta
 
 class Testquery(TestqueryTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    orders = anvil.server.call('orders')
-    
-    self.repeating_panel_1.items = orders
+    print('Start', datetime.now())
+ 
+
+    with Notification("Please wait... data loading "):
+        
+        orders = anvil.server.call('orders',self.text_box_1.text, self.drop_down_1.selected_value)
+        self.repeating_panel_1.items = orders
+        print('end', datetime.now())
+        # self.repeating_panel_1.items = orders
+        self.label_1.text ='No. of projects found = ' + str(len(self.repeating_panel_1.items ))
+     
+    print('end display', datetime.now())
     # Any code you write here will run before the form opens.
+
+  def text_box_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    with Notification("Please wait... data loading "):
+
+      orders = anvil.server.call('orders',self.text_box_1.text, self.drop_down_1.selected_value)
+      self.repeating_panel_1.items = orders
+      print('end', datetime.now())
+      # self.repeating_panel_1.items = orders
+      self.label_1.text ='No. of projects found = ' + str(len(self.repeating_panel_1.items ))
+    
+    
+    pass
+
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    with Notification("Please wait... data loading "):
+
+      orders = anvil.server.call('orders',self.text_box_1.text,self.drop_down_1.selected_value)
+      self.repeating_panel_1.items = orders
+      print('end', datetime.now())
+      # self.repeating_panel_1.items = orders
+      self.label_1.text ='No. of projects found = ' + str(len(self.repeating_panel_1.items ))
+    pass
+
+  
--- server_code/orders.py
+++ server_code/orders.py
@@ -8,6 +8,24 @@ import anvil.server
 
 
 @anvil.server.callable
-def orders():
-  orders = app_tables.sales_orders_all.search(q.fetch_only("order_no","project_name"))
+def orders(partname, stagegroup):
+  stages = ['Awaiting Sign-Off','Work In Progress - 4S', 'Pre-requisites in progress' ,'Ready for GoLive', 'Ready for UAT','Ready to Start','UAT WIP', 'On Hold','Order Approved', 'Order Submitted for Approval','Ordered']
+  if stagegroup == 'Work in Progress':
+        stages = ['Awaiting Sign-Off','Work In Progress - 4S', 'Pre-requisites in progress' ,'Ready for GoLive', 'Ready for UAT','Ready to Start','UAT WIP']
+  elif stagegroup == 'On Hold':
+        stages = ['On Hold']
+  elif stagegroup == 'Waiting to Start':
+    stages = [ 'Order Approved', 'Order Submitted for Approval','Ordered']
+  elif stagegroup == 'Closed':
+     stages = ['Closed']
+
+  
+  if partname:
+        orders = app_tables.sales_orders_all.search(q.fetch_only("order_no","project_name","stage"), project_name= q.ilike('%' + partname +'%'))
+  elif partname and stagegroup :
+      orders = app_tables.sales_orders_all.search(q.fetch_only("order_no","project_name","stage"), stage=q.any_of(*stages), project_name= q.ilike('%' + partname +'%'))
+  elif stagegroup :
+     orders = app_tables.sales_orders_all.search(q.fetch_only("order_no","project_name","stage"), stage=q.any_of(*stages))
+  elif not partname and not stagegroup:
+     orders = app_tables.sales_orders_all.search(q.fetch_only("order_no","project_name","stage"))
   return orders
