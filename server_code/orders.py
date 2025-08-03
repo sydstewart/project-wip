9b515a779985e3ad165e6987bcaff90c429ffff7
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2025-08-03 08:28:57 +0000

    Edited Testquery, new_searches and orders

--- server_code/orders.py
+++ server_code/orders.py
@@ -5,35 +5,23 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.secrets
 import anvil.server
-
+import pandas as pd
 
 @anvil.server.callable
 def orders(kwargs):
-#   stages = ['Awaiting Sign-Off','Work In Progress - 4S', 'Pre-requisites in progress' ,'Ready for GoLive', 'Ready for UAT','Ready to Start','UAT WIP', 'On Hold','Order Approved', 'Order Submitted for Approval','Ordered']
-#   if stagegroup == 'Work in Progress':
-#         stages = ['Awaiting Sign-Off','Work In Progress - 4S', 'Pre-requisites in progress' ,'Ready for GoLive', 'Ready for UAT','Ready to Start','UAT WIP']
-#   elif stagegroup == 'On Hold':
-#         stages = ['On Hold']
-#   elif stagegroup == 'Waiting to Start':
-#     stages = [ 'Order Approved', 'Order Submitted for Approval','Ordered']
-#   elif stagegroup == 'Closed':
-#      stages = ['Closed']
 
-# # Setup search dictionary
-#   kwargs ={}
-
-
-# #Project Name
-#   if partname:
-#      kwargs['project_name'] =  q.ilike('%' + partname +'%')
-
-# #stage
-#   if stagegroup:
-#     kwargs['stage'] =  q.any_of(*stages)  
   if kwargs:
-    orders = app_tables.sales_orders_all.search(q.fetch_only("order_no","project_name","stage"), **kwargs)
+    orders = app_tables.sales_orders_all.search( **kwargs)
   else:
-    orders = app_tables.sales_orders_all.search(q.fetch_only("order_no","project_name","stage"))
+    orders = app_tables.sales_orders_all.search()
+  # q.fetch_only("order_no","project_name","stage"
+  # convert to datframe to calc new columns
+  # X = pd.DataFrame.from_dict(orders)
+  # X['work_to_do'] = (X['order_value'] * ((100 - (X['percent_complete']))/100))
+  # # X['work_to_do'] = '£'+ str('{:,.2f}'.format(X['work_to_do'] ) )
+  # orders  = X.to_dict(orient='records')
+      
+  # print('orders', orders)
   return orders
   # if partname:
   #       orders = app_tables.sales_orders_all.search(q.fetch_only("order_no","project_name","stage"), project_name= q.ilike('%' + partname +'%'))
