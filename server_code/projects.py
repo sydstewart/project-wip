8bd4bd12fd64d004f8c94608db183854d199bdaf
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2025-08-07 11:45:58 +0000

    Edited Form9 and projects

--- server_code/projects.py
+++ server_code/projects.py
@@ -80,8 +80,153 @@ def get_orders_for_pivots(): # percent_complete,hi_percentage, assigned_to, cate
     schema = datatable_schema("sales_orders_all")
     dicts = schema.dump(orders, many=True)
     # print('dicts made',dicts)
+    X = pd.DataFrame.from_dict(dicts)
+  
+# print('Made dicts and dataframe')
+    X['order_value']= X['order_value'].map(float)
+    X['Total Order Value'] =X['order_value'].sum()
+# X['cumulative_orders'] =  X['order_value'].cusum()
+# print('Total Order Value from prepare pandas', X['Total Order Value'])
+    X['percent_complete'] = X['percent_complete'].fillna(0)
+# print('before',X['percent_complete'])
+    X['order_date'] = pd.to_datetime(X['order_date'], utc= True)  #, infer_datetime_format=True,  utc=True
+    X['date_formatted'] = X['order_date'].dt.strftime('%Y-%m-%d')
+    X['percent_complete']= X['percent_complete'].astype(int)
+
+    X['partially_invoiced_total'] = X['partially_invoiced_total'].fillna(0)
+    X['Work Completed'] =X['order_value'] * X['percent_complete']/100
+    X['Work To Do'] =X['order_value'] * (100 - X['percent_complete'])/100
+    X['Invoiced but NOT completed amount'] = X['partially_invoiced_total'] - X['Work Completed']
+    X['Invoiced but NOT completed amount']= X['Invoiced but NOT completed amount'].map(float)
+
+  
+    X['percent_complete'] = X['percent_complete'].map(int)
+    
+    X['order_value_formated'] = X['order_value'].map("£{:,.0f}".format)
+    X['partially_invoiced_total_formated'] = X['partially_invoiced_total'].map("£{:,.0f}".format)
+    X['Invoiced but NOT completed amount_formated'] = X['Invoiced but NOT completed amount'].map("£{:,.0f}".format)
+    X['Value yet to be invoiced'] = X['order_value']-X['partially_invoiced_total']  
+    X['Value yet to be invoiced_formated'] = X['Value yet to be invoiced'].map("£{:,.0f}".format)
+# utc_now = datetime.utcnow
+# naive_utc = datetime.utcnow()
+# today = datetime.utcnow() 
+# today = datetime.now(anvil.tz.tzutc())
+    today = datetime.today() #.strftime('%Y-%m-%d')
+
+# print('today', today)
+    X['today']= today
+    X['today']= pd.to_datetime(X.today,utc =True)
+    # print(X['today'])
+    X['order_date'] = pd.to_datetime(X.order_date,utc =True)
+    # print('order date',X['order_date'] )
+    X['days_elapsed'] = (X['today'] - X['order_date']).dt.days
+    # print('elapsed',X['days_elapsed']  )
+
+  #
+    X['Work Completed'] =X['order_value'] * X['percent_complete']/100
+    X['Work Completed_formated'] = X['Work Completed'].map("£{:,.0f}".format)
+    X['Work To Do'] =X['order_value'] * (100 - X['percent_complete'])/100
+    X['Work To Do_formated'] = X['Work To Do'].map("£{:,.0f}".format)
+    X['Order_Value_Value_Add _per Elapsed_day'] = X['order_value']//X['days_elapsed']
+    X['Work_completed_Value_Add _per Elapsed_day'] = X['Work Completed']//X['days_elapsed']
+    # X['Value Left to Do'] = (100 - X['percent_complete']) 
+    X['Year'] = X['order_date'].dt.year
+    X['Month']= X['order_date'].dt.month
+    X['Day']= X['order_date'].dt.day
+    X['Year'] = X['Year'].map(int)
+    X['Month'] = X['Month'].map(int)
+    X['Year Num'] =  X['Year'].map(int)
+    X['Month Num'] = X['Month'].map(int)
+
+    conditions = [
+      X['Month Num'] <= 2 ,
+      (X['Month Num'] > 2) & (X['Month Num'] <= 12),
+    
+    ]
+    dt = datetime.now()
+    print('start of categories',dt)
+    categories = [X['Year Num'] -1, X['Year Num']]
+    
+    # Use np.select() to create the new column
+    X['Fin Year'] = np.select(conditions, categories, default='Unknown') 
+    
+    conditions = [
+      X['Month Num'] <= 2 ,
+      (X['Month Num'] > 2) & (X['Month Num'] <= 12),
+    
+    ]
+    
+    categories = [X['Month Num'] + 10 , X['Month Num'] - 2 ]
+    
+    # Use np.select() to create the new column
+    X['Fin Month'] = np.select(conditions, categories, default='Unknown')
+    
+    # Group the stages into categories
+    conditions =[X['stage'] == 'On Hold',
+    
+                X['stage'] == 'Awaiting Sign-Off',
+                X['stage'] == 'Work In Progress - 4S', 
+                X['stage'] == 'Pre-requisites in progress' ,
+                X['stage'] == 'Ready for GoLive', 
+                X['stage'] == 'Ready for UAT',
+                X['stage'] == 'Ready to Start',
+                X['stage'] == 'UAT WIP',
+    
+                X['stage'] == 'Order Approved',
+                X['stage'] == 'Order Submitted for Approval',
+                X['stage'] == 'Ordered']
+  
+    categories = ['Projects on Hold', 
+                      'Project in Progress', 'Project in Progress', 'Project in Progress', 'Project in Progress','Project in Progress', 'Project in Progress','Project in Progress', \
+                      'Project waiting to Start', 'Project waiting to Start','Project waiting to Start']
+        
+    X['Stage Group'] = np.select(conditions, categories, default='Unknown')
+    dt = datetime.now()
+    print('end of categories',dt)
     return dicts
-    # dicts,dictspip,dictswts,dictsoh, X_media,  pivotsyd_to_markdown= prepare_pandas(dicts,percent_complete,hi_percentage,assigned_to, category, not_completed)
+# pivotsyd = pd.pivot_table(X, values = "order_value", index=['order_category'], aggfunc=('sum'), margins=True, margins_name='Total')
+# pivotsyd  = pivotsyd.fillna(0)
+# pivotsyd = pivotsyd.sort_values(by=['order_value'], ascending=False)
+# # print('pivotsyd',pivotsyd)
+# # fig = px.bar(pivotsyd, x='order_category', y='order_value')
+# pivotsyd['order_value']=pivotsyd['order_value'].apply('{:,}'.format)
+# # print("")
+# pivotsyd_to_markdown = pivotsyd.to_markdown()
+# # print(pivotsyd_to_markdown)
+# pd.set_option('display.max_columns', None)
+# # print('pivotsyd',pivotsyd)
+# dicts =X.to_dict(orient='records')
+# # For projects in progress
+# X1 = X[(X['Stage Group'] =='Project in Progress')] 
+# X1 = X1[['order_no','project_name','order_value', 'percent_complete', 'stage', 'waiting_on','Work To Do','Work To Do_formated','order_value_formated','partially_invoiced_total_formated','Value yet to be invoiced_formated','days_elapsed','Value yet to be invoiced', 'assigned_to']]
+# X1 = X1.sort_values(by='percent_complete',ascending=False)
+# # X2 = X1.sort_values(by='Value yet to be invoiced_formated',ascending=False)
+# dictspip= X1.to_dict(orient='records')
+# # print('No of projects in progress', len(dictspip))
+
+# # For projects waiting to start
+# X2 = X[(X['Stage Group'] =='Project waiting to Start')] 
+
+# X2 = X2[['order_no','project_name','order_value', 'percent_complete', 'stage', 'waiting_on','Work To Do','Work To Do_formated','order_value_formated','partially_invoiced_total_formated','Value yet to be invoiced_formated','days_elapsed','Value yet to be invoiced', 'assigned_to']]
+# X2= X2.sort_values(by='percent_complete',ascending=False)
+# # X2 = X1.sort_values(by='Value yet to be invoiced_formated',ascending=False)
+# dictswts= X2.to_dict(orient='records') 
+# # print('No of projects waiting to start', len(dictswts))
+
+# X3 = X[(X['Stage Group'] =='Projects on Hold')] 
+# X3 = X3[['order_no','project_name','order_value', 'percent_complete', 'stage', 'waiting_on','Work To Do','Work To Do_formated','order_value_formated','partially_invoiced_total_formated','Value yet to be invoiced_formated','days_elapsed','Value yet to be invoiced', 'assigned_to']]
+# X3= X3.sort_values(by='percent_complete',ascending=False)
+# # X2 = X1.sort_values(by='Value yet to be invoiced_formated',ascending=False)
+# dictsoh= X3.to_dict(orient='records') 
+# # print('No of projects on Hold', len(dictsoh))
+
+# # print('dictspip!!',dictspip)
+# X.to_csv('/tmp/X.csv') 
+# X_media = anvil.media.from_file('/tmp/X.csv', 'text/csv', 'X')
+# media_object = anvil.pdf.render_form('list_projects')
+
+
+
 #     X = pd.DataFrame.from_dict(dicts)
     # return dicts,dictspip, dictswts,dictsoh, X_media,  pivotsyd_to_markdown
   
